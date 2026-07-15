e682a6e0acbf4f7ed49c9a25110e39f36cadce7e
gemcad <pepepiks@gmail.com>
2026-07-15 19:36:03 +0200

    dodavanje student modela

---
+++ app/Models/Student.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $table = 'studenti';
+
+    protected $fillable = [
+        'ime',
+        'prezime',
+        'status',
+        'godiste',
+        'prosjek',
+        'stipendija',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'godiste' => 'integer',
+            'prosjek' => 'decimal:2',
+            'stipendija' => 'decimal:2',
+        ];
+    }
+}
